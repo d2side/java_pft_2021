c107c437baad5a8c77c4948ca91dee9eb4d36fcd
Kirill L <37121507+d2side@users.noreply.github.com>
2021-07-04 20:15:33 +0200

    Square / Rectangle classes were created

--- sandbox/src/main/java/ru/stq/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stq/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,26 @@ package ru.stq.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!!!");
+    hello();
 
+    Square s = new Square(9);
+
+    Rectangle r = new Rectangle(6, 4);
+
+    System.out.println("Square = " + area(s));
+
+    System.out.println("Rectangle = " + area(r));
+  }
+
+  public static void hello() {
+    System.out.println("Hello QA!");
+  }
+
+  public static double area(Square s) {
+    return s.l * s.l;
+  }
+
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 }	
---
+++ sandbox/src/main/java/ru/stq/pft/sandbox/Square.java
@@ -0,0 +1,10 @@
+package ru.stq.pft.sandbox;
+
+public class Square {
+
+  double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
