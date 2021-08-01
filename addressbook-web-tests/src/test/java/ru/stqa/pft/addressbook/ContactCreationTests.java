37da6ec0590d4dc6fbd87e7d734fd01c80e33df2
Kirill L <37121507+d2side@users.noreply.github.com>
2021-08-01 23:20:32 +0200

    atribtutes in methods were initialized as parameters

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,12 +1,10 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreationTests {
     private WebDriver driver;
@@ -19,24 +17,24 @@ public class ContactCreationTests {
         driver = new FirefoxDriver();
         baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         driver.get("http://localhost/addressbook/index.php");
         driver.findElement(By.name("user")).click();
         driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("user")).sendKeys(username);
         driver.findElement(By.name("pass")).click();
         driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.name("pass")).sendKeys(password);
         driver.findElement(By.id("LoginForm")).submit();
     }
 
     @Test
     public void testContactCreation() throws Exception {
         initContactCreation();
-        fillContactForm();
+        fillContactForm("ok","Lastname", "Nickname something", "brooklyn st");
         confirmContactCreation();
         returnToHomePage();
     }
@@ -49,20 +47,21 @@ public class ContactCreationTests {
         driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillContactForm() {
+    private void fillContactForm(String firstname, String lastname, String nickname, String address) {
         driver.findElement(By.name("firstname")).click();
         driver.findElement(By.name("firstname")).clear();
-        driver.findElement(By.name("firstname")).sendKeys("NameTest");
+        driver.findElement(By.name("firstname")).sendKeys(firstname);
         driver.findElement(By.name("lastname")).click();
         driver.findElement(By.name("lastname")).clear();
-        driver.findElement(By.name("lastname")).sendKeys("Lastname");
+        driver.findElement(By.name("lastname")).sendKeys(lastname);
         driver.findElement(By.name("nickname")).click();
         driver.findElement(By.name("nickname")).clear();
-        driver.findElement(By.name("nickname")).sendKeys("Nickname something");
+        driver.findElement(By.name("nickname")).sendKeys(nickname);
         driver.findElement(By.name("address")).click();
         driver.findElement(By.name("address")).clear();
-        driver.findElement(By.name("address")).sendKeys("brooklyn st");
+        driver.findElement(By.name("address")).sendKeys(address);
     }
+
 
     private void initContactCreation() {
         driver.findElement(By.linkText("add new")).click();
